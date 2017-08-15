9e8c616d6be8c7e9976502693991888d97f3477e
MARIA HRNDZ <maria.hrndz14@gmail.com>
2017-08-15 19:36:41 +0000

    Done.

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ function getFirstSelector(selector) {
   const sel1 = document.querySelector(selector);
 return sel1 }
 
-function nestedTarget()  {
-   const sel2 = document.querySelector();
-return sel2; }
+var lis = lis;
+function nestedTarget()  {         //  pull  .target out of #nested
+ //const lis = document.querySelector('#nested').document.querySelector('.target');
+  // const lis = document.getElementById('nested').document.getElementsByTagName('target');
+  const lis = document.querySelector('#nested .target');
+
+  //'<div class="target">...</div>';
+  //document.getElementById('app').querySelectorAll('.target #nested');
+
+ // var i = 0;
+ //for (let i = 0; i < lis.length; i++) {    lis[i].innerHTML = (i + 1).toString()  }
+
+ // id   --> #   document.querySelector('#nested')
+ // class --> .  document.querySelector('.target')
+return lis };
 
 function increaseRankBy(n)  {
    const sel3 = document.querySelector(n);
