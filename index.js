32ae3462dd1f8d8a4ae78c90c7d98d8e7bf7c06e
MARIA HRNDZ <maria.hrndz14@gmail.com>
2017-08-15 17:51:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+var selector = selector;
+
+function getFirstSelector(selector) {
+  const sel1 = document.querySelector(selector);
+return sel1 }
+
+function nestedTarget()  {
+   const sel2 = document.querySelector();
+return sel2; }
+
+function increaseRankBy(n)  {
+   const sel3 = document.querySelector(n);
+return sel3; }
+
+function deepestChild() {
+   const sel4 =  document.querySelectorAll();
+return sel4; }
